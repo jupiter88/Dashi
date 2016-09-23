f7a2e8bcd4bbd36724a0e997b280ab394c7f10d1
jupiter88 <mengjiang921018@hotmail.com>
2016-09-22 23:13:58 -0400

    First commit

---
+++ src/api/RpcParser.java
@@ -0,0 +1,55 @@
+package api;
+
+import java.io.IOException;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.*;
+
+public class RpcParser {
+	public static JSONObject parseInput(HttpServletRequest request) {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				jb.append(line);
+			}
+			reader.close();
+			return new JSONObject(jb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void writeOutput(HttpServletResponse response, JSONObject obj) {
+		try {
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.print(obj);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void writeOutput(HttpServletResponse response, JSONArray array) {
+		try {
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.print(array);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} 
+
+}
